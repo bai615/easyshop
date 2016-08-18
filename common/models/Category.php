82dacf992632c2b6f7d5ac995a51b7363120875b
baihua <baihua@24f094d0-34e2-4010-bb64-59cd5dc77d32>
2016-08-18 11:11:05 +0000

    common

---
+++ common/models/Category.php
@@ -0,0 +1,28 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+namespace common\models;
+
+use yii\db\ActiveRecord;
+
+/**
+ * Description of Category
+ *
+ * @author baihua <baihua_2011@163.com>
+ */
+class Category extends ActiveRecord {
+
+    /**
+     * 对应表名
+     * @return string
+     */
+//    public static function tableName() {
+//        return '{{%category}}';
+//    }
+
+}
---
+++ common/models/Goods.php
@@ -0,0 +1,20 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+namespace common\models;
+
+use yii\db\ActiveRecord;
+
+/**
+ * Description of Goods
+ *
+ * @author baihua <baihua_2011@163.com>
+ */
+class Goods extends ActiveRecord {
+
+}
