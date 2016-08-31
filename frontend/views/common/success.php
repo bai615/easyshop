964e78c8c336927b1f8b817dc91089df8682bd70
baihua <baihua@24f094d0-34e2-4010-bb64-59cd5dc77d32>
2016-08-31 13:15:52 +0000

    common views

---
+++ frontend/views/common/success.php
@@ -0,0 +1,13 @@
+<div class="container block_box">
+    <div class="message_box">
+        <p>
+            <i class="glyphicon glyphicon-ok-circle"></i><span class="message_text"><?php echo isset($message) ? $message : '操作成功'; ?></span>
+        </p>
+        <p>
+            您现在可以去：
+            <a class="blue" href="/">网站首页</a> | 
+            <a class="blue" href="/">我的订单</a> | 
+            <a class="blue" href="/">我的购物车</a>
+        </p>
+    </div>
+</div>
---
+++ frontend/views/common/warning.php
@@ -0,0 +1,14 @@
+<div class="container block_box">
+    <div class="message_box warning">
+        <p>
+            <i class="glyphicon glyphicon-warning-sign"></i><span class="message_text"><?php echo isset($message) ? $message : '操作错误'; ?></span>
+        </p>
+        <p>
+            您现在可以去：
+            
+            <a class="blue" href="/">网站首页</a> | 
+            <a class="blue" href="/">我的订单</a> | 
+            <a class="blue" href="/">我的购物车</a>
+        </p>
+    </div>
+</div>
