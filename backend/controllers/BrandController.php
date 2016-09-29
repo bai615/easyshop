1f32472cd47ec39ba0f824ae487aae7b322b36c5
baihua <baihua@24f094d0-34e2-4010-bb64-59cd5dc77d32>
2016-09-29 03:29:11 +0000

    品牌分类的添加、编辑、删除

--- backend/controllers/BrandController.php
+++ backend/controllers/BrandController.php
@@ -8,6 +8,8 @@
 
 namespace backend\controllers;
 
+use Yii;
+use yii\helpers\Url;
 use yii\data\Pagination;
 use common\models\BrandCategory;
 use common\models\Brand;
@@ -18,16 +20,16 @@ use common\models\Brand;
  * @author baihua <baihua_2011@163.com>
  */
 class BrandController extends BaseController {
-    
+
     /**
      * 品牌分类列表
      * @return type
      */
-    public function actionCategory(){
+    public function actionCategory() {
         $this->getBaseData();
         $data = BrandCategory::find();
         $pages = new Pagination(['totalCount' => $data->count(), 'pageSize' => '10']);
-        $model = $data->select(['id', 'name'])
+        $model = $data->select(['id', 'name', 'goods_category_id'])
             ->offset($pages->offset)
             ->limit($pages->limit)
             ->all();
@@ -36,6 +38,77 @@ class BrandController extends BaseController {
                 'model' => $model,
                 'pages' => $pages,
         ]);
+    }
+
+    /**
+     * 创建品牌分类
+     * @return type
+     */
+    public function actionCreateCategory() {
+        $this->getBaseData();
+        return $this->render('editCategory');
+    }
+
+    /**
+     * 编辑品牌分类
+     * @return type
+     */
+    public function actionEditCategory() {
+        $this->getBaseData('brand', 'category');
+        $categoryId = intval(Yii::$app->request->get('id'));
+        $categoryInfo = array();
+        if ($categoryId) {
+            $brandCategoryModel = new BrandCategory();
+            $categoryInfo = $brandCategoryModel->find()->where('id=:categoryId', [':categoryId' => $categoryId])->one();
+        }
+        return $this->render('editCategory', ['categoryInfo' => $categoryInfo]);
+    }
+
+    /**
+     * 保存品牌分类
+     */
+    public function actionCategorySave() {
+        //获得post值
+        $categoryId = intval(Yii::$app->request->post('id'));
+        $name = Yii::$app->request->post('name');
+        $goodsCategoryId = intval(Yii::$app->request->post('goods_category_id'));
+
+        if (empty($name)) {
+            $this->redirect(Url::to(['/brand/category-list']));
+        }
+
+        $categoryData = array(
+            'name' => $name,
+            'goods_category_id' => $goodsCategoryId
+        );
+        $brandCategoryModel = new BrandCategory();
+
+        //更新品牌分类
+        if ($categoryId) {
+            $brandCategoryModel->updateAll($categoryData, 'id=:categoryId', [':categoryId' => $categoryId]);
+        }
+        //添加品牌分类
+        else {
+            foreach ($categoryData as $key => $value) {
+                $brandCategoryModel->$key = $value;
+            }
+            $brandCategoryModel->save();
+        }
+        $this->redirect(Url::to(['/brand/category']));
+    }
+
+    /**
+     * 删除品牌分类
+     * @return type
+     */
+    public function actionDelCategory() {
+        $categoryId = intval(Yii::$app->request->get('id'));
+        $brandCategoryModel = new BrandCategory();
+        $brandCategoryInfo = $brandCategoryModel->find()->where('id=:categoryId', [':categoryId' => $categoryId])->one();
+        if ($brandCategoryInfo) {
+            $brandCategoryModel->deleteAll('id=:categoryId', [':categoryId' => $categoryId]);
+        }
+        $this->redirect(Url::to(['/brand/category']));
     }
 
     /**
