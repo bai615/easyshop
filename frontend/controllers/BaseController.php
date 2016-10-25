1969d33c2bebb759378f14126b10282b6d8ad552
baihua <baihua@24f094d0-34e2-4010-bb64-59cd5dc77d32>
2016-10-25 02:01:50 +0000

    用户自动登录实现

--- frontend/controllers/BaseController.php
+++ frontend/controllers/BaseController.php
@@ -5,6 +5,7 @@ namespace frontend\controllers;
 use Yii;
 use yii\web\Controller;
 use yii\helpers\Url;
+use common\utils\CommonTools;
 
 /**
  * Description of BaseController
@@ -19,6 +20,33 @@ class BaseController extends Controller {
         parent::init();
         $session = Yii::$app->session;
         $this->data['shopUserInfo'] = $session->get('shopUserInfo');
+
+        //自动登录检测
+        $cookieName = CommonTools::getAutoCookieName();
+        $cookies = Yii::$app->request->cookies;
+        //获取cookie自动登录信息
+        if (($cookie = $cookies->get($cookieName)) !== null) {
+            $cookieStr = $cookie->value;
+            //数据解密
+            $autoInfoStr = CommonTools::encrypt($cookieStr, 'D');
+            $autoInfoArr = explode('|', $autoInfoStr);
+            //获取当前IP值
+            $newIP = CommonTools::real_ip();
+            if (ip2long($newIP) != ip2long($autoInfoArr['1'])) {
+                //非同一个IP登录，需要重新登录
+            } else {
+                //进行自动登录操作
+                $userInfo = \common\models\User::findByUserName($autoInfoArr['0']);
+                $data = array(
+                    'userId' => $userInfo['id'],
+                    'userName' => $userInfo['username'],
+                    'head_ico' => $userInfo['head_ico'],
+                );
+                $this->data['shopUserInfo'] = $data;
+                //保存session信息
+                Yii::$app->session['shopUserInfo'] = $data;
+            }
+        }
     }
 
     /**
